3f2467c0c33667c8f8ed0301976e6ec91cbce74b
darwinmorales <darwin.morales.tech@gmail.com>
2016-11-04 05:34:27 +1100

    refactoring

--- src/App.js
+++ src/App.js
@@ -12,29 +12,77 @@ import './App.css';
 /////////////////
 
 // Container
-var App = React.createClass({
-    apiKey: '87dfa1c669eea853da609d4968d294be',
-    getInitialState: function () {
+// var App = React.createClass({
+//     apiKey: '87dfa1c669eea853da609d4968d294be',
+//     getInitialState: function () {
+//         return {searchTerm: "", searchUrl: ""};
+//     },
+//     handleKeyUp: function (e) {
+//         if (e.key === 'Enter' && this.state.searchTerm !== '') {
+//             var searchUrl = "search/multi?query=" + this.state.searchTerm + "&api_key=" + this.apiKey;
+//             this.setState({searchUrl: searchUrl});
+//         }
+//     },
+//
+//     handleChange: function (e) {
+//         this.setState({searchTerm: e.target.value});
+//     },
+//     render: function () {
+//         return (
+//             <div>
+//                 <header className="Header">
+//                     <Logo />
+//                     <Navigation />
+//                     <div id="search" className="Search">
+//                         <input onKeyUp={this.handleKeyUp} onChange={this.handleChange} type="search"
+//                                placeholder="Search for a title..." value={this.state.searchTerm}/>
+//                     </div>
+//                     <UserProfile />
+//                 </header>
+//                 <Hero />
+//                 <TitleList title="Search Results" url={this.state.searchUrl}/>
+//                 <TitleList title="Top TV picks for Jack" url='discover/tv?sort_by=popularity.desc&page=1'/>
+//                 <TitleList title="Trending now" url='discover/movie?sort_by=popularity.desc&page=1'/>
+//                 <TitleList title="Most watched in Horror" url='genre/27/movies?sort_by=popularity.desc&page=1'/>
+//                 <TitleList title="Sci-Fi greats" url='genre/878/movies?sort_by=popularity.desc&page=1'/>
+//                 <TitleList title="Comedy magic" url='genre/35/movies?sort_by=popularity.desc&page=1'/>
+//             </div>
+//         );
+//     }
+// });
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {searchTerm: "", searchUrl: ""};
+    }
+
+     apiKey: '87dfa1c669eea853da609d4968d294be';
+
+    getInitialState() {
         return {searchTerm: "", searchUrl: ""};
-    },
-    handleKeyUp: function (e) {
+    }
+
+    handleKeyUp(e) {
         if (e.key === 'Enter' && this.state.searchTerm !== '') {
             var searchUrl = "search/multi?query=" + this.state.searchTerm + "&api_key=" + this.apiKey;
             this.setState({searchUrl: searchUrl});
         }
-    },
+    }
 
-    handleChange: function (e) {
+    handleChange(e) {
         this.setState({searchTerm: e.target.value});
-    },
-    render: function () {
+    }
+
+    render() {
         return (
             <div>
                 <header className="Header">
                     <Logo />
                     <Navigation />
                     <div id="search" className="Search">
-                        <input onKeyUp={this.handleKeyUp} onChange={this.handleChange} type="search"
+                        <input onKeyUp={this.handleKeyUp.bind(this)} onChange={this.handleChange.bind(this)}
+                               type="search"
                                placeholder="Search for a title..." value={this.state.searchTerm}/>
                     </div>
                     <UserProfile />
@@ -49,8 +97,7 @@ var App = React.createClass({
             </div>
         );
     }
-});
-
+}
 
 export default App;
 
