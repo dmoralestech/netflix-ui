c0e45071f102fa70690e912a201574fa978ba705
dmorales <darwin.morales.tech@gmail.com>
2016-11-08 08:27:31 +1100

    add routing

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+/**
+ * Created by dmorales on 8/11/2016.
+ */
+
+import React from 'react'
+import App from './App'
+import MyApp from './MyApp'
+
+
+class Main extends React.Component {
+    
+    render() {
+        var Child;
+        
+        switch(this.props.route) {
+            case 'orig':
+                Child = App;
+            default:
+                Child = MyApp;
+        }
+        
+        return (
+            <div>
+                <Child/>
+            </div>
+        );
+    }
+    
+}
+
+export default Main
