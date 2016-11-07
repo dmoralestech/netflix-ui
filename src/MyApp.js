23426758a4a353e4bfde6bdce0af85f8c752bf0c
darwinmorales <darwin.morales.tech@gmail.com>
2016-11-07 20:14:40 +1100

    refactoring

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,3 +1,14 @@
 /**
  * Created by darwinmorales on 7/11/2016.
  */
+
+import React from 'react';
+
+class MyApp extends React.Component {
+    constructor() {
+
+    }
+
+}
+
+export default MyApp;
